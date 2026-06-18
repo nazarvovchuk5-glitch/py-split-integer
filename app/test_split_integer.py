372b69b72539cd09c2efbe5236f1c49ae17d212d
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-06-18 22:56:04 +0300

    Solution1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,8 +7,6 @@ from app.split_integer import split_integer
     (6, 2, [3, 3]),
     (17, 4, [4, 4, 4, 5]),
     (32, 6, [5, 5, 5, 5, 6, 6]),
-    (0, 1, [0]),
-    (0, 5, [0, 0, 0, 0, 0]),
     (1, 1, [1]),
     (10, 10, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
     (5, 5, [1, 1, 1, 1, 1]),
@@ -22,7 +20,7 @@ def test_split_integer_returns_correct_result(
 
 
 @pytest.mark.parametrize("value, number_of_parts", [
-    (8, 1), (6, 2), (17, 4), (32, 6), (0, 3), (7, 3), (10, 10),
+    (8, 1), (6, 2), (17, 4), (32, 6), (7, 3), (10, 10),
 ])
 def test_split_integer_returns_correct_length(
         value: int,
@@ -31,7 +29,7 @@ def test_split_integer_returns_correct_length(
 
 
 @pytest.mark.parametrize("value, number_of_parts", [
-    (8, 1), (6, 2), (17, 4), (32, 6), (0, 3), (7, 3), (10, 10),
+    (8, 1), (6, 2), (17, 4), (32, 6), (7, 3), (10, 10),
 ])
 def test_split_integer_sum_equals_value(
         value: int,
@@ -40,7 +38,7 @@ def test_split_integer_sum_equals_value(
 
 
 @pytest.mark.parametrize("value, number_of_parts", [
-    (8, 1), (6, 2), (17, 4), (32, 6), (0, 3), (7, 3), (10, 10),
+    (8, 1), (6, 2), (17, 4), (32, 6), (7, 3), (10, 10),
 ])
 def test_split_integer_max_min_difference_le_1(
         value: int,
@@ -50,7 +48,7 @@ def test_split_integer_max_min_difference_le_1(
 
 
 @pytest.mark.parametrize("value, number_of_parts", [
-    (8, 1), (6, 2), (17, 4), (32, 6), (0, 3), (7, 3), (10, 10),
+    (8, 1), (6, 2), (17, 4), (32, 6), (7, 3), (10, 10),
 ])
 def test_split_integer_is_sorted_ascending(
         value: int,
@@ -65,5 +63,4 @@ def test_split_integer_is_sorted_ascending(
 def test_split_integer_equal_parts_when_divisible(
         value: int,
         number_of_parts: int) -> None:
-    result = split_integer(value, number_of_parts)
-    assert len(set(result)) == 1
+    assert len(set(split_integer(value, number_of_parts))) == 1
