b358a9de8e483e17e60301aab6e31071764ec573
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-06-25 23:10:53 +0300

    Solution4

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -55,7 +55,9 @@ def test_parts_should_be_sorted_when_they_are_not_equal(
 
 
 @pytest.mark.parametrize("value, number_of_parts, expected", [
-    (4, 6, [0, 0, 1, 1, 1, 1]), (2, 3, [0, 1, 1]),
+    (4, 6, [0, 0, 1, 1, 1, 1]),
+    (2, 3, [0, 1, 1]),
+    (3, 5, [0, 0, 1, 1, 1]),
 ])
 def test_should_add_zeros_when_value_is_less_than_number_of_parts(
         value: int,
